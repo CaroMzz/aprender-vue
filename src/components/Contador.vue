d8c27acd78c14617803f70d017d7f647c5660921
Carolina <caromz2304@gmail.com>
2025-08-19 21:27:50 -0300

    Creación de un contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- Acá va el HTML -->
+
+  <p>{{ contador }}</p>
+  <button @click = "Incrementar">Incrementar</button>
+  <button @click = "Decrementar">Decrementar</button>
+</template>
+
+<script setup>
+  /*Acá va la lógica en JavaScript*/ 
+
+  import {ref} from 'vue'
+  const contador = ref(0)
+
+  const Incrementar = () =>{
+    contador.value++
+  }
+  const Decrementar = () =>{
+    contador.value--
+  }
+</script>
+
+<style scoped>
+  /*Acá va el CSS*/
+
+  p{
+    color: deeppink;
+  }
+</style>
